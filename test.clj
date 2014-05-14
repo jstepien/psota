10646dc8689a9ebd179b4ebabc21e9149bb6ecf3
Jan Stępień <jstepien@users.sourceforge.net>
2014-05-14 22:31:40 +0200

    Add a pending test case showing a recur bug

--- test.clj
+++ test.clj
@@ -22,6 +22,10 @@
          (println "Some tests failed!")
          (throw "fail")))))
 
+(defmacro ignore
+  [& exprs]
+  'true)
+
 (tests
 
   ;; seqs
@@ -44,6 +48,11 @@
   ;; for
   (= [[0 0] [0 1] [1 0] [1 1] [2 0] [2 1] [3 0] [3 1]]
      (for [x (range 4) y (range 2)] [x y]))
+
+  (ignore
+    ;; FIXME: Invoking recur in a fn accepting rest args does not work.
+    (let [f (fn [& xs] (when (seq xs) (recur (next xs))))]
+      (nil? (f 1 2 3))))
 
   ;; quasi quoting
   (eval `(= 'a# 'a#))
